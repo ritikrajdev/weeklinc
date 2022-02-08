ec2acb8f862bd2964212e22483fa9d1db76dcb7d
ritikrajdev <ritikrajdev761@gmail.com>
2022-02-08 17:39:54 +0530

    docs enhancement

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
-from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -10,27 +9,26 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Meet, Schedule
 from .serializers import MeetSerializer, MeetURLSerializer, ScheduleSerializer
-from .decorators import apply_filters
 
 
 class ScheduleViewSet(ModelViewSet):
+    queryset = Schedule.objects.none()
     serializer_class = ScheduleSerializer
     permission_classes = [IsAuthenticated]
     ordering_fields = ['name']
     filterset_fields = ordering_fields
 
-    @apply_filters
     def get_queryset(self):
         return self.request.user.schedules.all()
 
 
 class MeetViewSet(ModelViewSet):
+    queryset = Meet.objects.none()
     serializer_class = MeetSerializer
     permission_classes = [IsAuthenticated]
     ordering_fields = ['name', 'start_datetime', 'end_datetime']
     filterset_fields = ordering_fields + ['schedule']
 
-    @apply_filters
     def get_queryset(self):
         return Meet.objects.filter(schedule__in=self.request.user.schedules.all())
 
